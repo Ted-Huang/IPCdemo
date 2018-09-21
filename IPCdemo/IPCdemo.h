3040c955bc81569648294886a55c2d5ddd481d25
Ted <fw128ted@gmail.com>
2018-09-21 11:11:55 +0800

    first

---
+++ IPCdemo/IPCdemo.h
@@ -0,0 +1,32 @@
+
+// IPCdemo.h : PROJECT_NAME 應用程式的主要標頭檔
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "對 PCH 包含此檔案前先包含 'stdafx.h'"
+#endif
+
+#include "resource.h"		// 主要符號
+
+
+// CIPCdemoApp: 
+// 請參閱實作此類別的 IPCdemo.cpp
+//
+
+class CIPCdemoApp : public CWinApp
+{
+public:
+	CIPCdemoApp();
+
+// 覆寫
+public:
+	virtual BOOL InitInstance();
+
+// 程式碼實作
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CIPCdemoApp theApp;
